a64fb2cb3c635ec61753109e8d39dee049408032
Dinesh <mdinesh@micropyramid.com>
2019-11-13 23:33:28 +0530

    new branch

--- hungry.py
+++ hungry.py
@@ -2,4 +2,8 @@ hungry = input("are our hungry : ")
 if hungry=="yes":
     print("have your lunch")
 else:
-    print("do your home work")
+    thirsty = input("are you thirsty : ")
+    if thirsty=="yes":
+        print("drink water")
+    else:
+        print("do your work")
